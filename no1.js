43f786ef2448dc3773ec208727a299650e43076d
Arulmsms123 <arulmsms123@gmail.com>
2022-02-11 14:28:48 +0700

    Tugas mas Ian 2

---
+++ no1.js
@@ -0,0 +1,8 @@
+function minmax(Ahmad) {
+const Small = Math.min(...Ahmad);
+const Big = Math.max(...Ahmad);
+console.log(Small,Big);
+}
+minmax([1,2,3,4,5]);
+minmax([1])
+minmax([2334454,5])
---
+++ no2.js
@@ -0,0 +1,35 @@
+// function maxmin(array) {
+//     const jawa = array.filter(no=>{
+//         return no < 3
+//     })
+//     console.log(jawa);
+    
+// }
+// maxmin([1, 2, 3]);
+
+// function maxmin(array) {
+//     const jawa = array.filter(no=>{
+//         return no < 2
+//     })
+//     console.log(jawa);
+    
+// }
+// maxmin([1, 2, 3],2);
+
+// function maxmin(array) {
+//     const jawa = array.filter(no=>{
+//         return no < 1
+//     })
+//     console.log(jawa);
+    
+// }
+// maxmin([1, 2, 3],5);
+
+function maxmin(array) {
+    const jawa = array.filter(no=>{
+        return no = 3
+    })
+    console.log(jawa);
+    
+}
+maxmin([1, 2, 3],0);
